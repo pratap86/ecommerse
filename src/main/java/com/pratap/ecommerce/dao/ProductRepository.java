b720157be74b4ca4e73760f089b819e244b7e84a
pratap86 <narayanpratap86@gmail.com>
2021-12-31 19:46:29 +0530

    new query api introduced - findByProductCategoryId

--- src/main/java/com/pratap/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/pratap/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.pratap.ecommerce.dao;
 
 import com.pratap.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:5600/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Query method to fetch products by their category
+    Page<Product> findByProductCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
